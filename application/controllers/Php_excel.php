767bd85b462aa9a83e0981271a140b06673a708a
shrikant waghare <shrikantbro3886@gmail.com>
2023-07-27 21:31:44 +0530

    reading and import functionslity

--- application/controllers/Php_excel.php
+++ application/controllers/Php_excel.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Php_excel extends CI_Controller {
 
+	function __construct() 
+	{
+        parent::__construct();
+        $this->load->database();
+		//$this->load->model('UserModel', 'um');
+
+        header('Access-Control-Allow-Origin: *'); //--for cors error-----
+    }
 
 	public function index()
 	{
@@ -21,7 +29,7 @@ class Php_excel extends CI_Controller {
 
 		$sheet = $excel->getActiveSheet()->toArray(null,true,true,true);
 		//pre($excel->getActiveSheet());die;
-		pre($sheet);die;
+		//pre($sheet);die;
         $arrayCount = count($sheet); 
         for($i=2;$i<=$arrayCount;$i++)
         {                   
@@ -30,19 +38,18 @@ class Php_excel extends CI_Controller {
     }
 	public function test2()
 	{
-		//require 'vendor/autoload.php';
+		//---read multiple sheet-
+		$this->load->library('phpexcel');
+        //$this->load->library('PHPExcel/IOFactory.php');
+		$xl_obj = new PHPExcel();
+		$reader = PHPExcel_IOFactory::createReader('Excel2007');
+		$reader->setReadDataOnly(true);
+		$excel_Obj = $reader->load(FCPATH.'upload/excel_file/test_500.xlsx');
 
-		//use PhpOffice\PhpSpreadsheet\Spreadsheet;
-		//use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-		$this->load->library('PhpSpreadsheet\Spreadsheet');
-		$this->load->library('PhpSpreadsheet\Writer\Xlsx');
+		//  Read sheet 0
+		$worksheet=$excel_Obj->getSheet('0')->toArray(null,true,true,true);
+		pre($worksheet);
 
-		$spreadsheet = new Spreadsheet();
-		$activeWorksheet = $spreadsheet->getActiveSheet();
-		$activeWorksheet->setCellValue('A1', 'Hello World !');
-
-		$writer = new Xlsx($spreadsheet);
-		$writer->save('hello world.xlsx');
 	}
 	public function test3()
 	{
@@ -59,4 +66,60 @@ class Php_excel extends CI_Controller {
 		pre($sheet);die;
 
 	}
+	public function test4()
+	{
+		//---read data and save to table-
+		$this->load->library('phpexcel');
+        //$this->load->library('PHPExcel/IOFactory.php');
+		$xl_obj = new PHPExcel();
+		$reader = PHPExcel_IOFactory::createReader('Excel2007');
+		$reader->setReadDataOnly(true);
+		$excel_Obj = $reader->load(FCPATH.'upload/excel_file/test_500.xlsx');
+
+		//  Read sheet 0
+		$worksheet=$excel_Obj->getSheet('0');
+
+		$colomncount = $worksheet->getHighestDataColumn();
+		$rowcount = $worksheet->getHighestRow();
+		$colomncount_number=PHPExcel_Cell::columnIndexFromString($colomncount);
+
+		//pre($colomncount);
+		//pre($rowcount);
+
+		$insertquery='INSERT INTO `emp` (`first_name`, `last_name`,`gender`,`country`,`age`, `doj`) VALUES ';
+		$subquery='';
+
+
+		for($row=2; $row<= $rowcount - 1; $row++)
+		{
+			$subquery=$subquery.' (';
+			for($col=1;$col<$colomncount_number;$col++)
+			{
+				$cell_value = $worksheet->getCell(PHPExcel_Cell::stringFromColumnIndex($col).$row)->getValue();
+				
+				if($col == 6)
+				{					
+					$date = str_replace('/', '-', $cell_value);
+					$cell_value = date('Y-m-d', strtotime($date));
+				}
+				
+				$subquery=$subquery.'\''.$cell_value.'\',';
+			}
+			$subquery = substr($subquery, 0, strlen($subquery) - 1);
+			$subquery=$subquery.')'.' , ';
+		}
+
+		$insertquery=$insertquery.$subquery;
+		$insertquery= substr($insertquery,0,strlen($insertquery)-2);
+
+		
+		if($this->db->query($insertquery))
+		{
+			echo "Total ".$rowcount." Records Saved Successfully";
+		}
+
+		//pre($insertquery);
+
+	}
+
 }
--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2023 at 10:37 AM
+-- Generation Time: Jul 27, 2023 at 06:00 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -20,6 +20,128 @@ SET time_zone = "+00:00";
 --
 -- Database: `test`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp`
+--
+
+CREATE TABLE `emp` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `age` int(11) NOT NULL,
+  `doj` date NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `emp`
+--
+
+INSERT INTO `emp` (`id`, `first_name`, `last_name`, `gender`, `country`, `age`, `doj`, `created_date`) VALUES
+(1, 'Dulce', 'Abril', 'Female', 'United States', 32, '2017-10-15', '2023-07-27 15:58:37'),
+(2, 'Mara', 'Hashimoto', 'Female', 'Great Britain', 25, '2016-08-16', '2023-07-27 15:58:37'),
+(3, 'Philip', 'Gent', 'Male', 'France', 36, '2015-05-21', '2023-07-27 15:58:37'),
+(4, 'Kathleen', 'Hanner', 'Female', 'United States', 25, '2017-10-15', '2023-07-27 15:58:37'),
+(5, 'Nereida', 'Magwood', 'Female', 'United States', 58, '2016-08-16', '2023-07-27 15:58:37'),
+(6, 'Gaston', 'Brumm', 'Male', 'United States', 24, '2015-05-21', '2023-07-27 15:58:37'),
+(7, 'Etta', 'Hurn', 'Female', 'Great Britain', 56, '2017-10-15', '2023-07-27 15:58:37'),
+(8, 'Earlean', 'Melgar', 'Female', 'United States', 27, '2016-08-16', '2023-07-27 15:58:37'),
+(9, 'Vincenza', 'Weiland', 'Female', 'United States', 40, '2015-05-21', '2023-07-27 15:58:37'),
+(10, 'Fallon', 'Winward', 'Female', 'Great Britain', 28, '2016-08-16', '2023-07-27 15:58:37'),
+(11, 'Arcelia', 'Bouska', 'Female', 'Great Britain', 39, '2015-05-21', '2023-07-27 15:58:37'),
+(12, 'Franklyn', 'Unknow', 'Male', 'France', 38, '2017-10-15', '2023-07-27 15:58:37'),
+(13, 'Sherron', 'Ascencio', 'Female', 'Great Britain', 32, '2016-08-16', '2023-07-27 15:58:37'),
+(14, 'Marcel', 'Zabriskie', 'Male', 'Great Britain', 26, '2015-05-21', '2023-07-27 15:58:37'),
+(15, 'Kina', 'Hazelton', 'Female', 'Great Britain', 31, '2016-08-16', '2023-07-27 15:58:37'),
+(16, 'Shavonne', 'Pia', 'Female', 'France', 24, '2015-05-21', '2023-07-27 15:58:37'),
+(17, 'Shavon', 'Benito', 'Female', 'France', 39, '2017-10-15', '2023-07-27 15:58:37'),
+(18, 'Lauralee', 'Perrine', 'Female', 'Great Britain', 28, '2016-08-16', '2023-07-27 15:58:37'),
+(19, 'Loreta', 'Curren', 'Female', 'France', 26, '2015-05-21', '2023-07-27 15:58:37'),
+(20, 'Teresa', 'Strawn', 'Female', 'France', 46, '2015-05-21', '2023-07-27 15:58:37'),
+(21, 'Belinda', 'Partain', 'Female', 'United States', 37, '2017-10-15', '2023-07-27 15:58:37'),
+(22, 'Holly', 'Eudy', 'Female', 'United States', 52, '2016-08-16', '2023-07-27 15:58:37'),
+(23, 'Many', 'Cuccia', 'Female', 'Great Britain', 46, '2015-05-21', '2023-07-27 15:58:37'),
+(24, 'Libbie', 'Dalby', 'Female', 'France', 42, '2015-05-21', '2023-07-27 15:58:37'),
+(25, 'Lester', 'Prothro', 'Male', 'France', 21, '2017-10-15', '2023-07-27 15:58:37'),
+(26, 'Marvel', 'Hail', 'Female', 'Great Britain', 28, '2016-08-16', '2023-07-27 15:58:37'),
+(27, 'Angelyn', 'Vong', 'Female', 'United States', 29, '2015-05-21', '2023-07-27 15:58:37'),
+(28, 'Francesca', 'Beaudreau', 'Female', 'France', 23, '2017-10-15', '2023-07-27 15:58:37'),
+(29, 'Garth', 'Gangi', 'Male', 'United States', 41, '2016-08-16', '2023-07-27 15:58:37'),
+(30, 'Carla', 'Trumbull', 'Female', 'Great Britain', 28, '2015-05-21', '2023-07-27 15:58:37'),
+(31, 'Veta', 'Muntz', 'Female', 'Great Britain', 37, '2017-10-15', '2023-07-27 15:58:37'),
+(32, 'Stasia', 'Becker', 'Female', 'Great Britain', 34, '2016-08-16', '2023-07-27 15:58:37'),
+(33, 'Jona', 'Grindle', 'Female', 'Great Britain', 26, '2015-05-21', '2023-07-27 15:58:37'),
+(34, 'Judie', 'Claywell', 'Female', 'France', 35, '2016-08-16', '2023-07-27 15:58:37'),
+(35, 'Dewitt', 'Borger', 'Male', 'United States', 36, '2015-05-21', '2023-07-27 15:58:37'),
+(36, 'Nena', 'Hacker', 'Female', 'United States', 29, '2017-10-15', '2023-07-27 15:58:37'),
+(37, 'Kelsie', 'Wachtel', 'Female', 'France', 27, '2016-08-16', '2023-07-27 15:58:37'),
+(38, 'Sau', 'Pfau', 'Female', 'United States', 25, '2015-05-21', '2023-07-27 15:58:37'),
+(39, 'Shanice', 'Mccrystal', 'Female', 'United States', 36, '2015-05-21', '2023-07-27 15:58:37'),
+(40, 'Chase', 'Karner', 'Male', 'United States', 37, '2017-10-15', '2023-07-27 15:58:37'),
+(41, 'Tommie', 'Underdahl', 'Male', 'United States', 26, '2016-08-16', '2023-07-27 15:58:37'),
+(42, 'Dorcas', 'Darity', 'Female', 'United States', 37, '2015-05-21', '2023-07-27 15:58:37'),
+(43, 'Angel', 'Sanor', 'Male', 'France', 24, '2017-10-15', '2023-07-27 15:58:37'),
+(44, 'Willodean', 'Harn', 'Female', 'United States', 39, '2016-08-16', '2023-07-27 15:58:37'),
+(45, 'Weston', 'Martina', 'Male', 'United States', 26, '2015-05-21', '2023-07-27 15:58:37'),
+(46, 'Roma', 'Lafollette', 'Female', 'United States', 34, '2017-10-15', '2023-07-27 15:58:37'),
+(47, 'Felisa', 'Cail', 'Female', 'United States', 28, '2016-08-16', '2023-07-27 15:58:37'),
+(48, 'Demetria', 'Abbey', 'Female', 'United States', 32, '2015-05-21', '2023-07-27 15:58:37'),
+(49, 'Jeromy', 'Danz', 'Male', 'United States', 39, '2017-10-15', '2023-07-27 15:58:37'),
+(50, 'Rasheeda', 'Alkire', 'Female', 'United States', 29, '2016-08-16', '2023-07-27 15:58:37'),
+(51, 'Dulce', 'Abril', 'Female', 'United States', 32, '2017-10-15', '2023-07-27 15:58:37'),
+(52, 'Mara', 'Hashimoto', 'Female', 'Great Britain', 25, '2016-08-16', '2023-07-27 15:58:37'),
+(53, 'Philip', 'Gent', 'Male', 'France', 36, '2015-05-21', '2023-07-27 15:58:37'),
+(54, 'Kathleen', 'Hanner', 'Female', 'United States', 25, '2017-10-15', '2023-07-27 15:58:37'),
+(55, 'Nereida', 'Magwood', 'Female', 'United States', 58, '2016-08-16', '2023-07-27 15:58:37'),
+(56, 'Gaston', 'Brumm', 'Male', 'United States', 24, '2015-05-21', '2023-07-27 15:58:37'),
+(57, 'Etta', 'Hurn', 'Female', 'Great Britain', 56, '2017-10-15', '2023-07-27 15:58:37'),
+(58, 'Earlean', 'Melgar', 'Female', 'United States', 27, '2016-08-16', '2023-07-27 15:58:37'),
+(59, 'Vincenza', 'Weiland', 'Female', 'United States', 40, '2015-05-21', '2023-07-27 15:58:37'),
+(60, 'Fallon', 'Winward', 'Female', 'Great Britain', 28, '2016-08-16', '2023-07-27 15:58:37'),
+(61, 'Arcelia', 'Bouska', 'Female', 'Great Britain', 39, '2015-05-21', '2023-07-27 15:58:37'),
+(62, 'Franklyn', 'Unknow', 'Male', 'France', 38, '2017-10-15', '2023-07-27 15:58:37'),
+(63, 'Sherron', 'Ascencio', 'Female', 'Great Britain', 32, '2016-08-16', '2023-07-27 15:58:37'),
+(64, 'Marcel', 'Zabriskie', 'Male', 'Great Britain', 26, '2015-05-21', '2023-07-27 15:58:37'),
+(65, 'Kina', 'Hazelton', 'Female', 'Great Britain', 31, '2016-08-16', '2023-07-27 15:58:37'),
+(66, 'Shavonne', 'Pia', 'Female', 'France', 24, '2015-05-21', '2023-07-27 15:58:37'),
+(67, 'Shavon', 'Benito', 'Female', 'France', 39, '2017-10-15', '2023-07-27 15:58:37'),
+(68, 'Lauralee', 'Perrine', 'Female', 'Great Britain', 28, '2016-08-16', '2023-07-27 15:58:37'),
+(69, 'Loreta', 'Curren', 'Female', 'France', 26, '2015-05-21', '2023-07-27 15:58:37'),
+(70, 'Teresa', 'Strawn', 'Female', 'France', 46, '2015-05-21', '2023-07-27 15:58:37'),
+(71, 'Belinda', 'Partain', 'Female', 'United States', 37, '2017-10-15', '2023-07-27 15:58:37'),
+(72, 'Holly', 'Eudy', 'Female', 'United States', 52, '2016-08-16', '2023-07-27 15:58:37'),
+(73, 'Many', 'Cuccia', 'Female', 'Great Britain', 46, '2015-05-21', '2023-07-27 15:58:37'),
+(74, 'Libbie', 'Dalby', 'Female', 'France', 42, '2015-05-21', '2023-07-27 15:58:37'),
+(75, 'Lester', 'Prothro', 'Male', 'France', 21, '2017-10-15', '2023-07-27 15:58:37'),
+(76, 'Marvel', 'Hail', 'Female', 'Great Britain', 28, '2016-08-16', '2023-07-27 15:58:37'),
+(77, 'Angelyn', 'Vong', 'Female', 'United States', 29, '2015-05-21', '2023-07-27 15:58:37'),
+(78, 'Francesca', 'Beaudreau', 'Female', 'France', 23, '2017-10-15', '2023-07-27 15:58:37'),
+(79, 'Garth', 'Gangi', 'Male', 'United States', 41, '2016-08-16', '2023-07-27 15:58:37'),
+(80, 'Carla', 'Trumbull', 'Female', 'Great Britain', 28, '2015-05-21', '2023-07-27 15:58:37'),
+(81, 'Veta', 'Muntz', 'Female', 'Great Britain', 37, '2017-10-15', '2023-07-27 15:58:37'),
+(82, 'Stasia', 'Becker', 'Female', 'Great Britain', 34, '2016-08-16', '2023-07-27 15:58:37'),
+(83, 'Jona', 'Grindle', 'Female', 'Great Britain', 26, '2015-05-21', '2023-07-27 15:58:37'),
+(84, 'Judie', 'Claywell', 'Female', 'France', 35, '2016-08-16', '2023-07-27 15:58:37'),
+(85, 'Dewitt', 'Borger', 'Male', 'United States', 36, '2015-05-21', '2023-07-27 15:58:37'),
+(86, 'Nena', 'Hacker', 'Female', 'United States', 29, '2017-10-15', '2023-07-27 15:58:37'),
+(87, 'Kelsie', 'Wachtel', 'Female', 'France', 27, '2016-08-16', '2023-07-27 15:58:37'),
+(88, 'Sau', 'Pfau', 'Female', 'United States', 25, '2015-05-21', '2023-07-27 15:58:37'),
+(89, 'Shanice', 'Mccrystal', 'Female', 'United States', 36, '2015-05-21', '2023-07-27 15:58:37'),
+(90, 'Chase', 'Karner', 'Male', 'United States', 37, '2017-10-15', '2023-07-27 15:58:37'),
+(91, 'Tommie', 'Underdahl', 'Male', 'United States', 26, '2016-08-16', '2023-07-27 15:58:37'),
+(92, 'Dorcas', 'Darity', 'Female', 'United States', 37, '2015-05-21', '2023-07-27 15:58:37'),
+(93, 'Angel', 'Sanor', 'Male', 'France', 24, '2017-10-15', '2023-07-27 15:58:37'),
+(94, 'Willodean', 'Harn', 'Female', 'United States', 39, '2016-08-16', '2023-07-27 15:58:37'),
+(95, 'Weston', 'Martina', 'Male', 'United States', 26, '2015-05-21', '2023-07-27 15:58:37'),
+(96, 'Roma', 'Lafollette', 'Female', 'United States', 34, '2017-10-15', '2023-07-27 15:58:37'),
+(97, 'Felisa', 'Cail', 'Female', 'United States', 28, '2016-08-16', '2023-07-27 15:58:37'),
+(98, 'Demetria', 'Abbey', 'Female', 'United States', 32, '2015-05-21', '2023-07-27 15:58:37'),
+(99, 'Jeromy', 'Danz', 'Male', 'United States', 39, '2017-10-15', '2023-07-27 15:58:37');
 
 -- --------------------------------------------------------
 
@@ -52,7 +174,28 @@ INSERT INTO `location` (`id`, `country`, `state`, `city`) VALUES
 (15, 'India', 'Maharashtra', 'Thane'),
 (16, 'India', 'Maharashtra', 'Thane'),
 (17, 'India', 'Maharashtra', 'Thane'),
-(18, 'India', 'Maharashtra', 'Ambarnath');
+(18, 'India', 'Maharashtra', 'Ambarnath'),
+(19, 'India', 'Maharashtra', 'Karjat'),
+(20, 'India', 'Maharashtra', 'Thane'),
+(21, 'India', 'Maharashtra', 'Thane'),
+(22, 'India', 'Maharashtra', 'Thane'),
+(23, 'India', 'Maharashtra', 'Thane'),
+(24, 'India', 'Maharashtra', 'Thane'),
+(25, 'India', 'Maharashtra', 'Thane'),
+(26, 'India', 'Maharashtra', 'Thane'),
+(27, 'India', 'Maharashtra', 'Thane'),
+(28, 'India', 'Maharashtra', 'Thane'),
+(29, 'India', 'Maharashtra', 'Thane'),
+(30, 'India', 'Maharashtra', 'Thane'),
+(31, 'India', 'Maharashtra', 'Thane'),
+(32, 'India', 'Maharashtra', 'Thane'),
+(33, 'India', 'Maharashtra', 'Thane'),
+(34, 'India', 'Maharashtra', 'Thane'),
+(35, 'India', 'Maharashtra', 'Thane'),
+(36, 'India', 'Maharashtra', 'Thane'),
+(37, 'India', 'Maharashtra', 'Thane'),
+(38, 'India', 'Maharashtra', 'Thane'),
+(39, 'India', 'Maharashtra', 'Thane');
 
 -- --------------------------------------------------------
 
@@ -78,7 +221,7 @@ INSERT INTO `user` (`id`, `full_name`, `mobile_no`, `email`, `loc_id`, `file_nam
 (18, 'Shrikant Waghare', '7676565676', 'shrikant23@gmail.com', 4, 'pic_27560.jpg', '2023-07-01 06:13:17'),
 (19, 'Dinesh', '9898788787', 'testemail3232@gmail.com', 6, '', '2023-07-01 06:17:47'),
 (20, 'Akash', '9898543434', 'testemail326532@gmail.com', 7, 'pic_88392.jpg', '2023-07-03 06:19:18'),
-(22, 'Prakash', '9898543434', 'testemail3826532@gmail.com', 9, '', '2023-07-04 06:19:43'),
+(22, 'Prakash', '9898543434', 'testemail3826532@gmail.com', 9, 'pic_53800.jpg', '2023-07-04 06:19:43'),
 (23, 'Satish', '9898543434', 'testemail38626532@gmail.com', 10, '', '2023-07-04 06:19:58'),
 (24, 'Jayesh', '9898543434', 'testemail386296532@gmail.com', 11, 'pic_48176.jpg', '2023-07-06 06:20:16'),
 (25, 'Haresh', '9898543434', 'testemail54@gmail.com', 12, '', '2023-07-06 06:20:35'),
@@ -87,11 +230,38 @@ INSERT INTO `user` (`id`, `full_name`, `mobile_no`, `email`, `loc_id`, `file_nam
 (28, 'Bharat', '9898543434', 'testemail7874@gmail.com', 15, '', '2023-07-08 06:21:40'),
 (29, 'Kunal', '9898543434', 'testemail78794@gmail.com', 16, 'pic_48249.jpg', '2023-07-13 06:21:53'),
 (30, 'Atul', '9898543434', 'testemail878794@gmail.com', 17, 'pic_88025.jpg', '2023-07-13 06:22:05'),
-(31, 'Prashant', '9898543434', 'testemail8778794@gmail.com', 18, 'pic_20987.jpg', '2023-07-13 06:22:38');
+(31, 'Prashant', '9898543434', 'testemail8778794@gmail.com', 18, 'pic_20987.jpg', '2023-07-13 06:22:38'),
+(32, 'Bharti Waghare', '8987876765', 'bharti345@gmail.com', 19, 'pic_57468.jpg', '2023-07-15 06:03:42'),
+(33, 'Shrikant_1531', '9898543434', 'testemail877899758@gmail.com', 20, '', '2023-07-15 16:11:01'),
+(34, 'Shrikant_4306', '9898543434', 'testemail877878658@gmail.com', 21, '', '2023-07-15 16:11:05'),
+(35, 'Shrikant_56', '9898543434', 'testemail877837784@gmail.com', 22, '', '2023-07-15 16:11:06'),
+(36, 'Shrikant_1233', '9898543434', 'testemail877836698@gmail.com', 23, '', '2023-07-15 16:11:06'),
+(37, 'Shrikant_2951', '9898543434', 'testemail877898991@gmail.com', 24, '', '2023-07-15 16:11:07'),
+(38, 'Shrikant_959', '9898543434', 'testemail877853647@gmail.com', 25, '', '2023-07-15 16:11:07'),
+(39, 'Shrikant_3727', '9898543434', 'testemail877846010@gmail.com', 26, '', '2023-07-15 16:11:10'),
+(40, 'Shrikant_3901', '9898543434', 'testemail877843870@gmail.com', 27, '', '2023-07-15 16:11:11'),
+(41, 'Shrikant_1745', '9898543434', 'testemail877898158@gmail.com', 28, '', '2023-07-15 16:11:11'),
+(42, 'Shrikant_885', '9898543434', 'testemail877827400@gmail.com', 29, '', '2023-07-15 16:11:11'),
+(43, 'Shrikant_3657', '9898543434', 'testemail877864324@gmail.com', 30, '', '2023-07-15 16:11:11'),
+(44, 'Shrikant_1715', '9898543434', 'testemail877880997@gmail.com', 31, '', '2023-07-15 16:11:12'),
+(45, 'Shrikant_2980', '9898543434', 'testemail877838555@gmail.com', 32, '', '2023-07-15 16:11:12'),
+(46, 'Shrikant_4069', '9898543434', 'testemail877862807@gmail.com', 33, '', '2023-07-15 16:11:12'),
+(47, 'Shrikant_598', '9898543434', 'testemail877869657@gmail.com', 34, '', '2023-07-15 16:11:12'),
+(48, 'Shrikant_2204', '9898543434', 'testemail877847238@gmail.com', 35, '', '2023-07-15 16:11:12'),
+(49, 'Shrikant_4456', '9898543434', 'testemail877895585@gmail.com', 36, '', '2023-07-15 16:11:12'),
+(50, 'Shrikant_1088', '9898543434', 'testemail877829448@gmail.com', 37, '', '2023-07-15 16:11:13'),
+(51, 'Shrikant_2319', '9898543434', 'testemail877845457@gmail.com', 38, '', '2023-07-15 16:11:13'),
+(52, 'Shrikant_355', '9898543434', 'testemail877898660@gmail.com', 39, '', '2023-07-15 16:11:13');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `emp`
+--
+ALTER TABLE `emp`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `location`
@@ -111,16 +281,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `emp`
+--
+ALTER TABLE `emp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+
+--
 -- AUTO_INCREMENT for table `location`
 --
 ALTER TABLE `location`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
